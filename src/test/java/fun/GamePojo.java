d1d6e69226f582e7830d285bfcd755c8bb080805
Kutman <kzhutman@gmail.com>
2023-02-22 17:32:09 -0600

    Done with asessment

---
+++ src/test/java/fun/GamePojo.java
@@ -0,0 +1,11 @@
+package fun;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GamePojo {
+    private int id;
+    private String text;
+}
--- src/test/java/fun/GameTests.java
+++ src/test/java/fun/GameTests.java
@@ -1,6 +1,23 @@
 package fun;
 
-public class GameTests {
+import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.response.Response;
+import org.junit.Test;
 
+public class GameTests {
+    @Test
+    public void checkSchema(){
+
+
+        Response response = RestAssured.given()
+                .accept("application/json")
+                .contentType("application/json")
+                .when().get("http://localhost:8080/game?name=")
+                .then()
+                .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schema.json"))
+                .statusCode(200).extract().response();
+        GamePojo deserializedResponse=response.as(GamePojo.class);
+    }
 
 }
